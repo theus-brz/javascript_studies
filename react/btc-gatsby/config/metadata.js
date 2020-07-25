ee0d8192a22116c45dc292edf03449e8dc521f52
Matheus <matheusbrbs@gmail.com>
2020-07-25 20:00:31 -0300

    feat: gatsby plugins

---
+++ react/btc-gatsby/config/metadata.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: `Rocketseat`,
+  siteUrl: `https://www.rocketseat.com.br`,
+  description: `As melhores tecnologias em programação, direto ao ponto e do jeito certo.`,
+  social: [
+    {
+      name: `github`,
+      url: `https://github.com/rocketseat`,
+    },
+    {
+      name: `twitter`,
+      url: `https://twitter.com/rocketseat`,
+    },
+  ],
+};
--- react/btc-gatsby/gatsby-config.js
+++ react/btc-gatsby/gatsby-config.js
@@ -3,8 +3,40 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const siteMetadata = require('./config/metadata');
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat`,
+        short_name: `Rocketseat`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#7259c1`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7159c1`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-987654321", // Fictional ID
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
